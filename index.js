b97c332d1789d451fefa9856e99254b227745b90
Victor Nunez <VN135766@gmail.com>
2022-06-01 15:00:02 -0400

    working

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require ('fs');
+const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js')
+console.log("hello world")
 
 // TODO: Create an array of questions for user input
-const questions = () =>
+const questions = [
     {
-        return inquirer.prompt([
-            {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your project',
+        message: 'What is the title of your project?',
         validate: titleInput => {
             if (titleInput) {
                 return true;
             } else {
-                console.log ('Please enter title to continue');
+                console.log('Please enter title to continue');
                 return false;
             }
         }
-    }])}
+    }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
-function init() {};
+function init() { 
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log (userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    })
+};
 
 // Function call to initialize app
 init();
