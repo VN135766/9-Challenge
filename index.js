5a46e83f9f6afa20da6694550761ae603d6b1983
Victor Nunez <VN135766@gmail.com>
2022-06-01 13:55:47 -0400

    added 1 question

---
+++ index.js
@@ -0,0 +1,31 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require ('fs');
+const generatepage = require('./utils/generateMarkdown.js')
+
+// TODO: Create an array of questions for user input
+const questions = () =>
+    {
+        return inquirer.prompt([
+            {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log ('Please enter title to continue');
+                return false;
+            }
+        }
+    }])}
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
